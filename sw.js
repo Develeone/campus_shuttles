b2390bcab0338480a870ce9cb2acaedd59019861
develeone <babkov@awake.su>
2018-11-11 04:53:57 +1000

    icons

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 self.addEventListener('install', function(event) {
-    alert("install");
 });
 
 self.addEventListener('fetch', function(event) {
-    alert(event);
     event.respondWith(event);
 });
